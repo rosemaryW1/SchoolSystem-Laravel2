9b126dd284bb9cbbb9a8d1d63af6bd241cea3768
rosemaryW1 <rosemary.wakaba@starthmore.edu>
2022-12-19 14:36:23 +0300

    Added Edit functionality

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: pink;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
